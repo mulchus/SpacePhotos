a7922548bac93bedafbd13622c5a11b4831c8e9a
mulchus <mulch@list.ru>
2023-02-01 23:19:16 +0300

    Rename shadowing names

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import requests
 from pathlib import Path
 
 
-if __name__ == '__main__':
-
-    directory = './Images/123'
-    Path(directory).mkdir(parents=True, exist_ok=True)
-    filename = f'{directory}/hubble.jpeg'
+def get_image(urloffile, pathoffile):
+    Path(pathoffile).mkdir(parents=True, exist_ok=True)
+    file_name = urloffile.split('/')[-1]
 
     headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    response = requests.get(url, headers=headers)
+    response = requests.get(urloffile, headers=headers)
     response.raise_for_status()
 
-    with open(filename, 'wb') as file:
+    with open(f'{pathoffile}{file_name}', 'wb') as file:
         file.write(response.content)
+
+
+if __name__ == '__main__':
+    file_path = './Images/123/'  # first symbol is '.' if path is project directory continuation
+    file_url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    get_image(file_url, file_path)
