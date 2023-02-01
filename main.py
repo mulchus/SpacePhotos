35986c832f7479615771813fbac25c9721a3c98e
mulchus <mulch@list.ru>
2023-02-01 22:07:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+
+if __name__ == '__main__':
+    filename = './Photos/hubble.jpeg'
+    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
