0c6939af94fdfed5ed23a5e5f7928459fc3bc431
mulchus <mulch@list.ru>
2023-02-01 22:53:36 +0300

    User-Agent request appended

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
+from pathlib import Path
 
 
 if __name__ == '__main__':
-    filename = './Photos/hubble.jpeg'
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
-    response = requests.get(url)
+    directory = './Images/123'
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    filename = f'{directory}/hubble.jpeg'
+
+    headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
 
     with open(filename, 'wb') as file:
