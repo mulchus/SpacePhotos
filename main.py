c0e77038f7a6d75c1e4a29031bfc6d7273ad01ba
Юрий <mulch@list.ru>
2023-02-02 17:05:40 +0300

    Added selection of the number of images Added creation of a file name with a number

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import requests
 from pathlib import Path
 
 
-def get_image(urloffile, pathoffile):
+def get_image(urloffile, pathoffile, filename=''):
     Path(pathoffile).mkdir(parents=True, exist_ok=True)
-    file_name = urloffile.split('/')[-1]
+    if not filename:
+        filename = urloffile.rsplit('/', 1)[-1]
+        print(filename)
 
     headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
     response = requests.get(urloffile, headers=headers)
     response.raise_for_status()
 
-    with open(f'{pathoffile}{file_name}', 'wb') as file:
+    with open(f'{pathoffile}{filename}', 'wb') as file:
         file.write(response.content)
 
 
@@ -22,11 +24,11 @@ def takespaceximagelist(urloffile):
     if latestimageslist:
         return latestimageslist
     else:
-        urloffileall = (urloffile.replace('latest', 'past'))  # past по API выдает все предыдущие запуски
+        urloffileall = (urloffile.replace('latest', 'past'))  # 'past' outputs all previous launches
         response = requests.get(urloffileall)
         response.raise_for_status()
 
-        # формирование списка фото всех запусков, если их нет в последнем
+        # generating a list of photos of all launches, if they are not in the last one
         imagelist = []
         for spacexrecord in response.json():
             if spacexrecord['links']['flickr']['original']:
@@ -37,9 +39,17 @@ def takespaceximagelist(urloffile):
 if __name__ == '__main__':
     # file_path = './Images/123/'  # first symbol is '.' if path is project directory continuation
     # file_url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    file_path = './Images/SpaceX/'
-    file_url = 'https://api.spacexdata.com/v5/launches/latest'
-    spaceximagelist = takespaceximagelist(file_url)
 
-    for file_url in spaceximagelist[:3]:
-        get_image(file_url, file_path)
+    all_files_url = 'https://api.spacexdata.com/v5/launches/latest'
+    spaceximagelist = takespaceximagelist(all_files_url)
+
+    print(len(spaceximagelist))
+    firstimage = 100  # from the number of which image we download
+    numberimages = 5  # number of downloaded images
+
+    file_path = './Images/SpaceX/'
+    file_name = 'spacex'
+    # file_name = file_url.split('/')[-1]
+    for file_number, file_url in enumerate(spaceximagelist[firstimage-1:firstimage-1+numberimages]):
+        file_name = f'spacex_{file_number}.jpg'
+        get_image(file_url, file_path, file_name)
