f4a904645f2a6182d37e3166217e847ae23d92f9
mulchus <mulch@list.ru>
2023-02-05 01:29:17 +0300

    Created a two functions: for placements photo in telegram; for placements message in telegram;

--- mulcherbot.py
+++ mulcherbot.py
@@ -3,12 +3,22 @@ import constants
 
 
 bot = telegram.Bot(token=constants.bot_token)
+
+
 # print(bot.get_me())
 
-updates = bot.get_updates()
-print(updates[-1])
+# updates = bot.get_updates()
+# print(updates[-1])
+
+def bot_send_message(message, tchat_id):
+    bot.send_message(text=message, chat_id=tchat_id)
+    return
 
 # bot.send_message(text='Ну вот и новый бот родился!', chat_id=-1001585203348)
 
-# bot.send_photo(chat_id=-1001778853657, photo=open(f'{constants.work_dir}/Images/SpaceX/spacex_5.jpg', 'rb'))
-bot.send_photo(chat_id=-1001778853657, photo=open(f'{constants.work_dir}/Images/SpaceX/spacex_5.jpg', 'rb'))
+# bot.send_document(chat_id=-1001778853657, photo=open(f'{constants.work_dir}/Images/SpaceX/spacex_5.jpg', 'rb'))
+
+
+def bot_send_photo(tchat_id, path_to_file):
+    bot.send_photo(tchat_id, photo=open(path_to_file, 'rb'))
+    return
