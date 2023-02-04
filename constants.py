73329a712024e7b35d50a9dc578b7e899e171d5f
mulchus <mulch@list.ru>
2023-02-04 20:26:40 +0300

    Publish photo to Telegram channel.

--- constants.py
+++ constants.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 import os
+from os import path
 
 
 load_dotenv()
 nasa_api_key = os.getenv('NASA_API_KEY')
 bot_token = os.getenv('MULCHERBOT')
+
+work_dir = path.dirname(__file__)
--- mulcherbot.py
+++ mulcherbot.py
@@ -3,9 +3,12 @@ import constants
 
 
 bot = telegram.Bot(token=constants.bot_token)
-print(bot.get_me())
+# print(bot.get_me())
 
 updates = bot.get_updates()
-print(updates[0])
+print(updates[-1])
 
-bot.send_message(text='Ну вот и новый бот родился!', chat_id=-1001585203348)
+# bot.send_message(text='Ну вот и новый бот родился!', chat_id=-1001585203348)
+
+# bot.send_photo(chat_id=-1001778853657, photo=open(f'{constants.work_dir}/Images/SpaceX/spacex_5.jpg', 'rb'))
+bot.send_photo(chat_id=-1001778853657, photo=open(f'{constants.work_dir}/Images/SpaceX/spacex_5.jpg', 'rb'))
