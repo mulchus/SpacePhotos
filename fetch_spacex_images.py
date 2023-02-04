1937161680152436e0cb14fda43ea6e399a9b9b4
mulchus <mulch@list.ru>
2023-02-04 14:55:34 +0300

    Add saving SpaceX by ID in command line

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -2,21 +2,35 @@ import requests
 from pathlib import Path
 from os import path
 from urllib import parse
+import argparse
 
 
 def main():
-    id_of_launch = input('Введите ID запуска: ')
-    all_images_url = f'https://api.spacexdata.com/v5/launches/{id_of_launch}'  # 5eb87d42ffd86e000604b384
+    parserSpaceX = argparse.ArgumentParser(description='Ввод ID запуска для SpaceX (по умолчанию - последний запуск)')
+    parserSpaceX.add_argument(
+        '-id',
+        help='загрузка фото по введенному после -id коду запуска SpaceX'
+    )
+
+    id_launch = parserSpaceX.parse_args().id
+
+    if not id_launch:
+        response = requests.get('https://api.spacexdata.com/v5/launches/latest')
+        response.raise_for_status()
+        id_launch = response.json()['id']
+    print(id_launch)
+
+    all_images_url = f'https://api.spacexdata.com/v5/launches/{id_launch}'  # 5eb87d42ffd86e000604b384
     response = requests.get(all_images_url)
     response.raise_for_status()
 
     images_list = response.json()['links']['flickr']['original']
 
-    file_path = './Images/SpaceX/'  # first symbol is '.' if path is project directory continuation
+    file_path = f'{path.dirname(__file__)}/Images/SpaceX/'  # first symbol is '.' if path is project directory continuation
     Path(file_path).mkdir(parents=True, exist_ok=True)
 
     file_name_pattern = 'spacex_'
-
+    numbers_of_file = 0
     for file_number, file_url in enumerate(images_list):
         file_ext = path.splitext(parse.urlsplit(file_url).path)[1]
         file_name = f'{file_name_pattern}{file_number + 1}{file_ext}'
@@ -27,5 +41,10 @@ def main():
 
         with open(f'{file_path}{file_name}', 'wb') as file:
             file.write(response.content)
+        numbers_of_file += 1
 
-    print(f'Скачивание запуска ID {id_of_launch} завершено\n')
+    print(f'Скачивание запуска ID {id_launch} завершено\n Скачано {numbers_of_file} фото')
+
+
+if __name__ == '__main__':
+    main()
