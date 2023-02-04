134431e79bd927df7cc8d35f57c74326714fc59f
mulchus <mulch@list.ru>
2023-02-05 01:29:53 +0300

    Added chat_id, pause_beetwen_posts

--- constants.py
+++ constants.py
@@ -7,4 +7,8 @@ load_dotenv()
 nasa_api_key = os.getenv('NASA_API_KEY')
 bot_token = os.getenv('MULCHERBOT')
 
+chat_id = -1001778853657
+
 work_dir = path.dirname(__file__)
+
+pause_beetwen_posts = 14400  # pause for post images in chanel
