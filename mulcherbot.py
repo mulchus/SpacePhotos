5294a966048c91b294153325ec83bba0ee11ec17
mulchus <mulch@list.ru>
2023-02-05 15:11:03 +0300

    Added two function kast chat - for learning

--- mulcherbot.py
+++ mulcherbot.py
@@ -5,10 +5,17 @@ import constants
 bot = telegram.Bot(token=constants.bot_token)
 
 
-# print(bot.get_me())
+Adef last_chat_id():
+    return bot.get_updates()[-1]['message']['chat']['id']
 
+
+def last_update():
+    return bot.get_updates()[-1]
+
+# print(bot.get_me())
 # updates = bot.get_updates()
 # print(updates[-1])
+
 
 def bot_send_message(message, tchat_id):
     bot.send_message(text=message, chat_id=tchat_id)
@@ -22,3 +29,11 @@ def bot_send_message(message, tchat_id):
 def bot_send_photo(tchat_id, path_to_file):
     bot.send_photo(tchat_id, photo=open(path_to_file, 'rb'))
     return
+
+
+def main():
+    last_chat_id()
+
+
+if __name__ == '__main__':
+    main()
