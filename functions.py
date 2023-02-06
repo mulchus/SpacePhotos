e11181b63feaa1ac7cdcdad30cb783447c43be18
mulchus <mulch@list.ru>
2023-02-07 00:27:58 +0300

    Update enumerate

--- functions.py
+++ functions.py
@@ -6,12 +6,10 @@ import os
 
 def file_save(images, file_path, file_name_pattern, payload=''):
     numbers_of_file = 0
-    for file_number, file_url in enumerate(images):
+    for file_number, file_url in enumerate(images, start=1):
         file_ext = path.splitext(parse.urlsplit(file_url).path)[1]
-        file_name = f'{file_name_pattern}{file_number + 1}{file_ext}'
-
-        headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
-        response = requests.get(file_url, headers=headers, params=payload)
+        file_name = f'{file_name_pattern}{file_number}{file_ext}'
+        response = requests.get(file_url, params=payload)
         response.raise_for_status()
 
         with open(f'{file_path}{file_name}', 'wb') as file:
