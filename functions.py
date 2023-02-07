f4b77deea02a458a6bac5861cec06d50b59c9a03
Юрий <mulch@list.ru>
2023-02-07 12:29:59 +0300

    Delete path_of_files_in_dir

--- functions.py
+++ functions.py
@@ -4,11 +4,11 @@ from urllib import parse
 import os
 
 
-def file_save(images, file_path, file_name_pattern, payload=''):
+def file_save(images, file_path, payload=''):
     numbers_of_file = 0
     for file_number, file_url in enumerate(images, start=1):
         file_ext = path.splitext(parse.urlsplit(file_url).path)[1]
-        file_name = f'{file_name_pattern}{file_number}{file_ext}'
+        file_name = f'{file_number}{file_ext}'
         response = requests.get(file_url, params=payload)
         response.raise_for_status()
 
@@ -24,8 +24,3 @@ def path_of_files(start_dir):  # make a list of paths for all files in start dir
         for filename in files:
             file_paths.append(os.path.abspath(os.path.join(folder, filename)))
     return file_paths
-
-
-def path_of_files_in_dir(directory):  # make a list of paths for all files in directory
-    onlyfiles = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    return onlyfiles
