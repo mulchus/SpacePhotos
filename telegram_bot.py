66c23b443612e92ace22d09981f01be347eac71f
Юрий <mulch@list.ru>
2023-02-08 14:12:41 +0300

    Introduced a check for safe file opening

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,4 +3,5 @@ import telegram
 
 def bot_send_photo(telegram_bot_token, telegram_chat_id, path_to_file):
     bot = telegram.Bot(token=telegram_bot_token)
-    bot.send_photo(telegram_chat_id, photo=open(path_to_file, 'rb'))
+    with open(path_to_file, 'rb') as file:
+        bot.send_photo(telegram_chat_id, photo=file)
