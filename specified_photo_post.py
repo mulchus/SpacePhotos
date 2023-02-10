6c2b1cbb9e564c76c0f10d3fd70bba7bd76431fd
Юрий <mulch@list.ru>
2023-02-10 13:42:09 +0300

    Update functions argv to argparse

--- specified_photo_post.py
+++ specified_photo_post.py
@@ -1,4 +1,4 @@
-from sys import argv
+import argparse
 import os
 import functions
 import telegram_bot
@@ -10,11 +10,16 @@ def main():
     load_dotenv()
     telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
     telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
-    try:
-        path = argv[1]
-    except IndexError:
-        path = os.path.join(os.getcwd(), 'Images')
-        print('Не указан путь к папке с файлами!')
+    parser_image = argparse.ArgumentParser(description='Публикация фото по конкретному пути')
+    parser_image.add_argument(
+        'path',
+        nargs='?',
+        default=os.path.join(os.getcwd(), 'Images'),
+        help='директория с фото (по умолчанию - ПУТЬ_К_ПАПКЕ_СО_СКРИПТОМ/Images)'
+    )
+
+    path = parser_image.parse_args().path
+
     if os.path.isfile(path):
         telegram_bot.bot_send_photo(telegram_bot_token, telegram_chat_id, path)
     else:
