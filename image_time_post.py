b8cd3a7f47d73311e9f9f75611ce80c09345fbe1
Юрий <mulch@list.ru>
2023-02-10 13:32:48 +0300

    Update functions argv to argparse

--- image_time_post.py
+++ image_time_post.py
@@ -1,4 +1,4 @@
-from sys import argv
+import argparse
 import time
 from os import listdir, path
 import os
@@ -7,32 +7,36 @@ from random import shuffle
 from dotenv import load_dotenv
 
 
-SPECIAL_PAUSE=14400
-
-
 def main():
     load_dotenv()
     telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
     telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
-    try:
-        directory = argv[1] if argv[1] else os.getcwd()
-    except IndexError:
-        print('Не указан путь к папке с файлами!')
-        directory = os.path.join(os.getcwd(), 'Images', 'SpaceX')
-    try:
-        pause = int(argv[2])
-    except IndexError:
-        print(f'По умолчанию задана пауза = {SPECIAL_PAUSE}!')
-        pause = SPECIAL_PAUSE
+
+    parser_image = argparse.ArgumentParser(description='Публикация фото из заданной директории по алгоритму')
+    parser_image.add_argument(
+        'directory',
+        nargs='?',
+        default=os.path.join(os.getcwd(), 'Images', 'SpaceX'),
+        help='директория с фото (по умолчанию - ПУТЬ_К_ПАПКЕ_СО_СКРИПТОМ/Images/SpaceX)'
+    )
+    parser_image.add_argument(
+        'pause',
+        nargs='?',
+        default=14400,
+        help='задержка между публикациями в секундах (по умолчанию 14400 сек)'
+    )
+
+    directory = parser_image.parse_args().directory
+    pause = parser_image.parse_args().pause
 
     onlyfiles = [f for f in listdir(directory) if path.isfile(path.join(directory, f))]
 
     while True:
         for file in onlyfiles:
             telegram_bot.bot_send_photo(telegram_bot_token, telegram_chat_id, os.path.join(directory, file))
-            time.sleep(pause) if pause == SPECIAL_PAUSE else time.sleep(3)  # хитрые паузы согласно заданию
+            time.sleep(pause) if pause == 14400 else time.sleep(3)  # хитрые паузы согласно заданию
         shuffle(onlyfiles)
-        time.sleep(pause) if pause != SPECIAL_PAUSE else 0  # хитрые паузы согласно заданию
+        time.sleep(pause) if pause != 14400 else 0  # хитрые паузы согласно заданию
 
 
 if __name__ == '__main__':
