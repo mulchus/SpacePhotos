ae4adb316b548dd91235010797142978099af3b6
Юрий <mulch@list.ru>
2023-02-10 12:32:48 +0300

    Deleted from .env special pause Added directory by default

--- image_time_post.py
+++ image_time_post.py
@@ -7,31 +7,32 @@ from random import shuffle
 from dotenv import load_dotenv
 
 
+SPECIAL_PAUSE=14400
+
+
 def main():
     load_dotenv()
-    special_pause = int(os.environ['SPECIAL_PAUSE'])
     telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
     telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     try:
         directory = argv[1] if argv[1] else os.getcwd()
     except IndexError:
-        directory = ''
         print('Не указан путь к папке с файлами!')
-        exit()
+        directory = os.path.join(os.getcwd(), 'Images', 'SpaceX')
     try:
         pause = int(argv[2])
     except IndexError:
-        print(f'По умолчанию задана пауза = {special_pause}!')
-        pause = special_pause
+        print(f'По умолчанию задана пауза = {SPECIAL_PAUSE}!')
+        pause = SPECIAL_PAUSE
 
     onlyfiles = [f for f in listdir(directory) if path.isfile(path.join(directory, f))]
 
     while True:
         for file in onlyfiles:
-            telegram_bot.bot_send_photo(telegram_bot_token, telegram_chat_id, f'{directory}{file}')
-            time.sleep(pause) if pause == special_pause else time.sleep(3)  # хитрые паузы согласно заданию
+            telegram_bot.bot_send_photo(telegram_bot_token, telegram_chat_id, os.path.join(directory, file))
+            time.sleep(pause) if pause == SPECIAL_PAUSE else time.sleep(3)  # хитрые паузы согласно заданию
         shuffle(onlyfiles)
-        time.sleep(pause) if pause != special_pause else 0  # хитрые паузы согласно заданию
+        time.sleep(pause) if pause != SPECIAL_PAUSE else 0  # хитрые паузы согласно заданию
 
 
 if __name__ == '__main__':
