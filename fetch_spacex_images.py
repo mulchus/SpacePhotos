9d5e3c0ff7090c9810ae452dcfdd213b9ab372f9
mulchus <mulch@list.ru>
2023-02-12 16:24:29 +0300

    Renamed some variables and added a request error check

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -4,32 +4,36 @@ import argparse
 import functions
 
 
-def get_all_images_urls(id_launch, ):
-    response = requests.get(f'https://api.spacexdata.com/v5/launches/{id_launch}')
-    response.raise_for_status()
-    images_urls = response.json()['links']['flickr']['original']
-    return images_urls
+def get_all_images_urls(launch_id):
+    try:
+        response = requests.get(f'https://api.spacexdata.com/v5/launches/{launch_id}')
+        response.raise_for_status()
+        images_urls = response.json()['links']['flickr']['original']
+        return images_urls
+    except requests.exceptions.HTTPError as e:
+        print(f'Указан неверный ID запуска.\nОшибка {e}')
+        exit()
 
 
 def main():
-    parser_spacex = argparse.ArgumentParser(description='Ввод ID запуска для SpaceX (по умолчанию - последний запуск)')
-    parser_spacex.add_argument(
+    spacex_parser = argparse.ArgumentParser(description='Ввод ID запуска для SpaceX (по умолчанию - последний запуск)')
+    spacex_parser.add_argument(
         'id',
         default='latest',
         nargs='?',
         help='загрузка фото по введенному коду запуска SpaceX'
     )
 
-    id_launch = parser_spacex.parse_args().id
-    images_urls = get_all_images_urls(id_launch)
+    launch_id = spacex_parser.parse_args().id
+    images_urls = get_all_images_urls(launch_id)
 
     images_dir = Path.cwd() / 'Images' / 'SpaceX'
     Path(images_dir).mkdir(parents=True, exist_ok=True)
     file_path = Path.joinpath(images_dir, 'spacex_')
 
-    numbers_of_file = functions.save_file(images_urls, file_path)
+    files_count = functions.save_file(images_urls, file_path)
 
-    print(f'Скачивание запуска ID {id_launch} завершено\n Скачано {numbers_of_file} фото')
+    print(f'Скачивание запуска ID {launch_id} завершено\n Скачано {files_count} фото')
 
 
 if __name__ == '__main__':
